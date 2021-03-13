83495fa73daab4d5f74b45447bb43583581142b6
Stanley, Ezra <sockpuppet24@ufl.edu>
2021-03-13 15:28:06 -0500

    got slider plugin working

---
+++ js/main.js
@@ -0,0 +1,17 @@
+  $(function () {
+
+    $("#slider4").responsiveSlides({
+        auto: false,
+        pager: false,
+        nav: true,
+        speed: 500,
+        namespace: "callbacks",
+        before: function () {
+          $('.events').append("<li>before event fired.</li>");
+        },
+        after: function () {
+          $('.events').append("<li>after event fired.</li>");
+        }
+      });
+
+    });
